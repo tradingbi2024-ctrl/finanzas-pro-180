e70014dafd4b5b397cee23c31f7c01cf60b08ec6
tradingbi2024-ctrl <tradingbi2024@gmail.com>
2025-12-06 19:12:10 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
-migrate = Migrate()
+# Convención para nombres de constraints
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
+
 
 def init_db(app):
     db.init_app(app)
-    migrate.init_app(app, db)
+    with app.app_context():
+        db.create_all()
+    return db
